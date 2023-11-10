36e636807ad123efd971331f6d68cb40c78b83f0
andybeet <22455149+andybeet@users.noreply.github.com>
2023-11-10 16:32:34 -0500

    completed example make_rmd script to demonstrate the list objects required from github parser

--- R/make_rmd.R
+++ R/make_rmd.R
@@ -9,28 +9,109 @@
 # this list created after parsing issue
 listobject <- list()
 listobject$name <- "more forage fish"
+listobject$indicatorname <- "more_forage_fish"
+listobject$family <- "family"
 listobject$description <- "a really long description"
-listobject$family <- "the family of indicators"
+listobject$contributors <- "contribs"
+listobject$affiliations <- "affiliations"
+listobject$whatsthis <- "what is this"
+listobject$visualizations <- "visuals"
+listobject$indicatorStats <- "indicator stats"
+listobject$implications <- "implications"
+listobject$poc <- "poc"
 
 
 make_rmd <- function(listobject){
   
   # line for the indicator name
-  indicator_name <- listobject$name
-  # create filenmae based on indicator name. Replace all spaces with underscore
-  filename <- gsub("\\s+","_",indicator_name)
+  #indicator_name <- listobject$name
+  # create filename based on indicator name. Replace all spaces with underscore
+  #filename <- gsub("\\s+","_",indicator_name)
+  filename <- listobject$indicatorname
   
   # create rmd with name of indicator
   con <- file(here::here("chapters",paste0(filename,".rmd")),open="w")
      
   # start to create the Rmd
-  cat(paste0("# ",stringr::str_to_title(indicator_name)),append=T,fill=T,file=con)    
+  #cat(paste0("# ",stringr::str_to_title(indicator_name)),append=T,fill=T,file=con)    
+  cat(paste0("# ",listobject$name),append=T,fill=T,file=con)    
   cat("",append=T,fill=T,file=con) # add space
   cat(paste0("**Description**: ",listobject$description),append=T,fill=T,file=con)
   cat("",append=T,fill=T,file=con) # add space
   cat(paste0("**Indicator family**: ",listobject$family),append=T,fill=T,file=con)
-
-  #etc
+  cat("",append=T,fill=T,file=con) # add space
+  cat(paste0("**Contributor(s)**: ",listobject$contributors),append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  cat(paste0("**Affiliations**: ",listobject$affiliations),append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  # chunk script
+  cat("```{r echo=FALSE}",append=T,fill=T,file=con)
+  cat("knitr::opts_chunk$set(echo = F)",append=T,fill=T,file=con)
+  cat("library(ecodata)",append=T,fill=T,file=con)
+  cat("```",append=T,fill=T,file=con)
+  
+  cat("## What is this?",append=T,fill=T,file=con)
+  cat(listobject$whatsthis,append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  cat("### Visualizations",append=T,fill=T,file=con)
+  cat(listobject$visualizations,append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  ### section to run plot functions from ecodata
+  
+  
+  
+  cat("### Indicator statistics ",append=T,fill=T,file=con)
+  cat(listobject$indicatorStats,append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  # r chunk for autostats
+  cat(paste0("```{r autostats_",filename,"}"),append=T,fill=T,file=con)
+  cat("# Either from Contributor or ecodata",append=T,fill=T,file=con)
+  cat("```",append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  
+  cat("### Implications",append=T,fill=T,file=con)
+  cat(listobject$implications,append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  
+  cat("## Get the data",append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  cat(paste0("**Point of contact**: [",listobject$poc,"](",listobject$poc,"){.email}"),append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  cat(paste0("**ecodata name**: `ecodata::",listobject$indicatorname,"`"),append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  cat("**variable names**",append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  # r code chunk to make table
+  cat(paste0("```{r vars_",listobject$indicatorname,"}"),append=T,fill=T,file=con)
+  cat("# Pull all var names",append=T,fill=T,file=con)
+  cat(paste0("vars <- ecodata::",listobject$indicatorname," |>"),append=T,fill=T,file=con)
+  cat("   dplyr::select(Var, Units) |>",append=T,fill=T,file=con)
+  cat("   dplyr::distinct()",append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  cat("DT::datatable(vars)",append=T,fill=T,file=con) # add space
+  cat("```",append=T,fill=T,file=con)
+  
+  
+  
+  cat("**tech-doc link**",append=T,fill=T,file=con)
+  
+  cat(paste0("<https://noaa-edab.github.io/tech-doc/",listobject$indicatorname,".html>"),append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
+  
+  cat("## References",append=T,fill=T,file=con)
+  cat("",append=T,fill=T,file=con) # add space
+  
   
   #close the connection
   close(con)
