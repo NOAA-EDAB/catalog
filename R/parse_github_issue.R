8c24d39cd6e8c8ec3e6eaa8dbba707ecc1d1f10d
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2023-11-17 14:42:16 -0500

    Initial gh_parser catalog chapter creation
    Reworked gh_parser to ensure compatibility with make_Rmd function. Completed new workflow for catalog page generation, beginning with gh_parser function and ending with make_Rmd function. (See chapters/more_forage_fish.rmd for results)

--- R/parse_github_issue.R
+++ R/parse_github_issue.R
@@ -5,41 +5,17 @@
 #' 
 
 # Define repo information, pull issues from GH API and subset 'submissions'
-repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues'
-issues <- jsonlite::fromJSON(repo)
-indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
-submissions <- issues$number[indices]
+#repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues'
+#issues <- jsonlite::fromJSON(repo)
+#indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
+#submissions <- issues$number[indices]
 
-# Iterate over issues and generate named objects for each issue field
-for (issuenum in submissions) {
-  issue <- jsonlite::fromJSON(paste0(repo,"/",issuenum))
-  body <- issue$body
-  body_ss <- strsplit(body,"### ")
-  body_ul <- unlist(body_ss)
-
-# Split data name and create econame variable for naming conventions
-  if (grepl("\\n\\n",body_ul[4])) {
-    econame <- unlist(strsplit(body_ul[4],"\n\\n")) 
-  } else {
-    econame <- unlist(strsplit(body_ul[4],"\r\n\r\n"))
-  }
-  
-# Iterate over each line of issue and create object for each field  
-  for (linenum in 1:18) {
-    if (grepl("\\n\\n",body_ul[linenum])) {
-      body_rf <- unlist(strsplit(body_ul[linenum],"\n\\n")) 
-    } else {
-      body_rf <- unlist(strsplit(body_ul[linenum],"\r\n\r\n"))
-    }
-    
-    assign(paste0(econame[-1],"_",body_rf[1]), toString(body_rf[-1]))
-  }
-}
-
+### Everything below should be the parsing script
 ### WRITE A FUNCTION THAT TAKES ISSUENUM, LINENUM AS VARIABLES AND PRODUCES LISTOBJECT
-repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues'
+repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues' # Manually select repo (for now)
+issuenum <- 23 # Manually select repo for function. Initial catalog build will use loop
 
-gh_parser <- function(repo,issuenum,linenum){
+gh_parser <- function(linenum){
   issue <- jsonlite::fromJSON(paste0(repo,"/",issuenum))
   body <- issue$body
   body_ss <- strsplit(body,"### ")
@@ -54,19 +30,20 @@ gh_parser <- function(repo,issuenum,linenum){
   return(toString(body_rf[-1]))
 }
 
-### The gh_parser function should output the below listobject
+### Generate listobject using gh_parser
 listobject <- list()
-listobject$indicatorname <- "more forage fish"
+listobject$indicatorname <- gh_parser(4)
 listobject$ecodataname <- "more_forage_fish"  # add to template (dropdown)
 listobject$family <- "family" # add to template (dropdown)
-listobject$description <- "a really long description"
-listobject$contributors <- "contribs"
+listobject$description <- gh_parser(5)
+listobject$contributors <- gh_parser(15)
 listobject$affiliations <- "affiliations" # add to template
-listobject$whatsthis <- "what is this"
-listobject$visualizations <- "visuals"
-listobject$indicatorStatsSpatial <- "spatial"
-listobject$indicatorStatsTemporal <- "temporal"
-listobject$implications <- "implications"
-listobject$poc <- "poc"
+listobject$whatsthis <- gh_parser(6)
+listobject$visualizations <- gh_parser(7)
+listobject$indicatorStatsSpatial <- gh_parser(9)
+listobject$indicatorStatsTemporal <- gh_parser(10)
+listobject$implications <- gh_parser(8)
+listobject$poc <- gh_parser(16)
 
-### gh_parser can be saved as a standalone function and used by another script/function, if needed
+### Test make_Rmd.R using above inputs
+make_rmd(listobject)
