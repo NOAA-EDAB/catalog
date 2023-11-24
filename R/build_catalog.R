1bc3680b62f43104b29e2bffffc21d50b55745c4
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2023-11-24 12:19:43 -0500

    Added build_catalog script
    Added build_catalog.R which calls on gh_parser and make_rmd functions to generate catalog chapters. Moved code from both github_parse_issue.R and make_rmd.R to build_catalog.R.

---
+++ R/build_catalog.R
@@ -0,0 +1,40 @@
+#' Build catalog using make_rmd and gh_parser functions
+#' 
+#' Process GitHub issues and convert into listobject used by make_Rmd function
+#' 
+#' @param issuenum value. manually entered to select issue for parsing
+#' 
+#' @return 
+
+# Source required functions
+source(here::here("R/parse_github_issue.R"))
+source(here::here("R/make_rmd.R"))
+
+# Define repo information, pull issues from GH API and subset 'submissions'
+repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues'
+issues <- jsonlite::fromJSON(repo)
+indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
+submissions <- issues$number[indices]
+
+# Manual issue selection
+issuenum <- 37 # Manually select repo for function. Initial catalog build will use loop
+
+#for (issuenum in submissions) {
+### Generate listobject using gh_parser
+listobject <- list()
+listobject$indicatorname <- gh_parser(issuenum,4)
+listobject$ecodataname <- gh_parser(issuenum,4)  # add to template (dropdown)
+listobject$family <- "family" # add to template (dropdown)
+listobject$description <- gh_parser(issuenum,5)
+listobject$contributors <- gh_parser(issuenum,15)
+listobject$affiliations <- "affiliations" # add to template
+listobject$whatsthis <- gh_parser(issuenum,6)
+listobject$visualizations <- gh_parser(issuenum,7)
+listobject$indicatorStatsSpatial <- gh_parser(issuenum,9)
+listobject$indicatorStatsTemporal <- gh_parser(issuenum,10)
+listobject$implications <- gh_parser(issuenum,8)
+listobject$poc <- gh_parser(issuenum,16)
+
+### Test make_Rmd.R using above inputs
+make_rmd(listobject)
+#}
--- R/parse_github_issue.R
+++ R/parse_github_issue.R
@@ -6,17 +6,6 @@
 #' 
 #' @return 
 
-
-# Define repo information, pull issues from GH API and subset 'submissions'
-#repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues'
-#issues <- jsonlite::fromJSON(repo)
-#indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
-#submissions <- issues$number[indices]
-
-# Inputs for testing
-repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues' # Manually select repo (for now)
-issuenum <- 23 # Manually select repo for function. Initial catalog build will use loop
-
 # Function below
 gh_parser <- function(issuenum,linenum){
   issue <- jsonlite::fromJSON(paste0(repo,"/",issuenum))
@@ -32,21 +21,3 @@ gh_parser <- function(issuenum,linenum){
   
   return(toString(body_rf[-1]))
 }
-
-### Generate listobject using gh_parser
-listobject <- list()
-listobject$indicatorname <- gh_parser(issuenum,4)
-listobject$ecodataname <- "more_forage_fish"  # add to template (dropdown)
-listobject$family <- "family" # add to template (dropdown)
-listobject$description <- gh_parser(issuenum,5)
-listobject$contributors <- gh_parser(issuenum,15)
-listobject$affiliations <- "affiliations" # add to template
-listobject$whatsthis <- gh_parser(issuenum,6)
-listobject$visualizations <- gh_parser(issuenum,7)
-listobject$indicatorStatsSpatial <- gh_parser(issuenum,9)
-listobject$indicatorStatsTemporal <- gh_parser(issuenum,10)
-listobject$implications <- gh_parser(issuenum,8)
-listobject$poc <- gh_parser(issuenum,16)
-
-### Test make_Rmd.R using above inputs
-make_rmd(listobject)
