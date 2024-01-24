9ded35aeb3136ea59b48bfee846d271ff80f4cd8
andybeet <22455149+andybeet@users.noreply.github.com>
2024-01-24 17:36:24 -0500

    added function to pull all issues and use this list to operate on to avoid the GitHub limit of API requests per hour

--- R/build_rmd_from_issue.R
+++ R/build_rmd_from_issue.R
@@ -11,35 +11,54 @@
 # Source required functions
 source(here::here("R/parse_single_issue.R"))
 source(here::here("R/make_rmd.R"))
+library(ecodata)
 
-build_rmd_from_issue <- function(issuenum = 1) {
+issueData <- readRDS(here::here("data-raw/submissionIssueNumbers.rds"))
 
+pull_all_issues <- function() {
   # Define repo information, pull issues from GH API and subset 'submissions'
   repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues'
+  # pull all issues and select all submission issues
+  issueData <- list()
+  # pulls 100 issues
+  repopull <- paste0(repo,"?per_page=100")
+  issues <- jsonlite::fromJSON(repopull)
+  # make sure it has the "submission" tag associated with it
+  indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
+  submissions <- issues$number[indices]
+  # save pull to rds for debugging
+  issueData$issues <- issues
+  issueData$submissions <- submissions
+  saveRDS(issueData,here::here("data-raw/submissionIssueNumbers.rds"))
+  
+  return(issueData)
+  
+}
+
+build_rmd_from_issue <- function(issueData,issuenum = 1) {
+
 
   if (!is.null(issuenum)) {
-    repoissue <- paste0('https://api.github.com/repos/NOAA-EDAB/catalog/issues/',issuenum)
-    issues <- jsonlite::fromJSON(repoissue)
-    if (issues$labels$name == "submission") {
+#    repoissue <- paste0('https://api.github.com/repos/NOAA-EDAB/catalog/issues/',issuenum)
+#    issues <- jsonlite::fromJSON(repoissue)
+    if (issuenum %in% issueData$submissions) {
+    #if (issues$labels$name == "submission") {
       submissions <- issuenum
     } else {
       stop("This issue number is not a submission issue and can not be parsed")
     }
   } else {
     # pull all issues and select all submissions
-    issues <- jsonlite::fromJSON(repo)
-    # make sure it has the "submission" tag associated with it
-    indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
-    submissions <- issues$number[indices]
+    submissions <- issueData$submissions
   }
-  
   
   
   # pull data
   for (issuenum in submissions) {
     message(paste0("Issue number: ",issuenum))
+    
     ### Pull issue from GitHub API
-    parsedIssue <- parse_single_issue(repo,issuenum)
+    parsedIssue <- parse_single_issue(issueData,issuenum)
     
     
     ### Generate listobject using gh_parser
