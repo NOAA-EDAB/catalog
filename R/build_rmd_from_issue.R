415aef443e6c46a110c1b7cf0115cbba17d5d0a3
andybeet <22455149+andybeet@users.noreply.github.com>
2024-01-25 17:23:06 -0500

    added code to parse checkboxes and keep only those checked

--- R/build_rmd_from_issue.R
+++ R/build_rmd_from_issue.R
@@ -21,7 +21,7 @@ pull_all_issues <- function() {
   # pull all issues and select all submission issues
   issueData <- list()
   # pulls 100 issues
-  repopull <- paste0(repo,"?per_page=100")
+  repopull <- paste0(repo,"?per_page=200")
   issues <- jsonlite::fromJSON(repopull)
   # make sure it has the "submission" tag associated with it
   indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
@@ -66,14 +66,26 @@ build_rmd_from_issue <- function(issueData,issuenum = 1) {
     listobject$dataname <- parsedIssue$`### Data Name (This will be the displayed title in Catalog)`
     listobject$indicatorname <- parsedIssue$`### Indicator Name (as exists in ecodata)`
     listobject$description <- parsedIssue$`### Data Description`
-    listobject$family <- parsedIssue$`### Family (Which group is this indicator associated with?)`
+    
+    ## Parse checkboxes 
+    family <- parsedIssue$`### Family (Which group is this indicator associated with?)`
+    members <- unlist(stringr::str_extract_all(family,"- \\[X\\]\\s+[a-zA-z ]+"))
+    members <- paste0(members,"\r\n",collapse="")
+    
+    listobject$family <- members
     listobject$contributors <- parsedIssue$`### Data Contributors`
     listobject$affiliations <- parsedIssue$`### Affiliation`
     listobject$whatsthis <- parsedIssue$`### Introduction to Indicator (Please explain your indicator)`
     listobject$visualizations <- parsedIssue$`### Key Results and Visualization`
     listobject$indicatorStatsSpatial <- parsedIssue$`### Spatial Scale`
     listobject$indicatorStatsTemporal <- parsedIssue$`### Temporal Scale`
-    listobject$synthesisTheme <- parsedIssue$`### Synthesis Theme`
+    
+    ## Parse checkboxes 
+    synthesis <-  parsedIssue$`### Synthesis Theme`
+    members <- unlist(stringr::str_extract_all(synthesis,"- \\[X\\]\\s+[a-zA-z ]+"))
+    members <- paste0(members,"\r\n",collapse="")
+    
+    listobject$synthesisTheme <- members
     listobject$implications <- parsedIssue$`### Implications`
     listobject$poc <- parsedIssue$`### Point(s) of Contact`
     listobject$defineVariables <- parsedIssue$`### Define Variables`
