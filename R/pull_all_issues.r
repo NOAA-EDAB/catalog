2b5c73449383cb447118e24ae51389b8f0aa595c
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-07-29 15:44:07 -0400

    fix: rewrite API issue pull to accomodate more than 100 issues

--- R/pull_all_issues.r
+++ R/pull_all_issues.r
@@ -1,7 +1,7 @@
 #' Pulls all issues from GitHub API
 #'
 #' Issues that we are interested in have a "submission" tag associated with them.
-#' This submission tag is assigned in GitHub to the the issue Template 
+#' This submission tag is assigned in GitHub to the the issue Template
 #' so when a submission is entered it is automatically tagged
 #'
 #'
@@ -9,24 +9,56 @@
 #' \item{issueData}{a list of all issues . Each element is an issue}
 #' \item{submissions}{the issue numbers that are "submissions"}
 
-
-
 pull_all_issues <- function() {
-  # Define repo information, pull issues from GH API and subset 'submissions'
-  repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues'
-  # pull all issues and select all submission issues
-  issueData <- list()
-  # pulls 100 issues
-  repopull <- paste0(repo,"?per_page=200")
-  issues <- jsonlite::fromJSON(repopull)
-  # make sure it has the "submission" tag associated with it
-  indices <- which(unlist(lapply(issues$labels,function(x) {if(length(x$name)==0){F}else{x$name=="submission"}})))
+  # Configure API pull
+  repo <- 'https://api.github.com/repos/NOAA-EDAB/catalog/issues' # Link to target repo
+  issues <- list() # Initialize empty list object
+  per_page <- 100 # Determine issues per page (max 100)
+  page <- 1 # Initialize page object
+
+  repeat {
+    # Define target URL using page specifications
+    repopull <- paste0(repo, "?per_page=", per_page, "&page=", page)
+
+    # Pull issues from current page
+    # Each loop will pull from the next page
+    current_page_issues <- jsonlite::fromJSON(repopull)
+
+    # End the loop when page contains 0 issues
+    if (length(current_page_issues) == 0) {
+      break
+    }
+
+    # Store issues from current page as list
+    issues <- dplyr::bind_rows(issues, current_page_issues)
+
+    # Increment page number
+    page <- page + 1
+  }
+
+  # Find issue numbers labeled "submission"
+  indices <- which(unlist(lapply(issues$labels, function(x) {
+    if (length(x$name) == 0) {
+      F
+    } else {
+      x$name == "submission"
+    }
+  })))
+
+  # Subset issues labeled "submission"
   submissions <- issues$number[indices]
-  # save pull to rds for debugging
+
+  # Initialize issueData object to store API pull
+  issueData <- list()
+
+  # Save all issue data
   issueData$issues <- issues
+
+  # Save "submission" issue data
   issueData$submissions <- submissions
-  saveRDS(issueData,here::here("data-raw/submissionIssueNumbers.rds"))
-  
+
+  # Save pull data as rds for debugging
+  saveRDS(issueData, here::here("data-raw/submissionIssueNumbers.rds"))
+
   return(issueData)
-  
 }
